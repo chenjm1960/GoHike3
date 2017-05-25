52d68a14645757e818148926926ff6146a07744a
James Chen <JamesMChen@Alecs-MacBook-Pro.local>
2017-05-24 23:57:49 -0700

    version one of installing a speed bar using CorePlot

--- GoHike/ViewController.swift
+++ GoHike/ViewController.swift
@@ -29,7 +29,16 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     var startLocation: CLLocation!
     var lastLocation: CLLocation!
     var updateCount = 0
+    var runSpeed: Double = 0.000
     
+    // using CorePlot
+    @IBOutlet weak var speedBarView: CPTGraphHostingView!
+    var propAnnotation: CPTPlotSpaceAnnotation?
+    // plot* defines a bar for displaying speed
+    var plot1: CPTBarPlot!
+    let BarWidth = 0.25
+    let BarInitialX = 0.25
+    ///////////////////////////
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var totalDistMiles2: UILabel!
@@ -119,20 +128,18 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        //let location = locations[0]
+        let location = locations[0]
         
-        /*
+        
         // 1. dist method using (speed * time)
         
         if location.speed >= 0.0 {
             
-            totalDistanceMeters += Double(location.speed)
+            runSpeed = location.speed
+            initPlot()
             
         }
-        self.speedLabel.text = String(location.speed)
-        self.totalDist.text = String(totalDistanceMeters)
-        self.totalDistMiles.text = String(format: "%.4f",(totalDistanceMeters * 0.0006214))
-        */
+        
         
         // set map to center and focus on your location
         // use if loop to update map just 5 times
@@ -169,5 +176,212 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         self.totalDistMiles2.text = String(format: "%.4f",(totalDistanceMeters2 * 0.0006214))
         
     }
+    
+    ////////////////////// CorePlot Functions /////////////////////
+    
+    func initPlot() {
+        configureHostView()
+        configureGraph()
+        configureChart()
+        configureAxes()
+    }
+    
+    func configureHostView() {
+        speedBarView.allowPinchScaling = false
+    }
+    
+    func configureGraph() {
+        // 1 - Create the graph
+        let graph = CPTXYGraph(frame: speedBarView.bounds)
+        graph.plotAreaFrame?.masksToBorder = false
+        speedBarView.hostedGraph = graph
+        
+        
+        
+        // 2 - Configure the graph
+        graph.apply(CPTTheme(named: CPTThemeName.plainWhiteTheme))
+        graph.fill = CPTFill(color: CPTColor.clear())
+        graph.paddingBottom = 5.0
+        graph.paddingLeft = 5.0
+        graph.paddingTop = 0.0
+        graph.paddingRight = 2.0
+        
+        // 3 - Set up styles
+        let titleStyle = CPTMutableTextStyle()
+        titleStyle.color = CPTColor.black()
+        titleStyle.fontName = "HelveticaNeue-Bold"
+        titleStyle.fontSize = 8.0
+        titleStyle.textAlignment = .center
+        graph.titleTextStyle = titleStyle
+        
+        let title = "Speed"
+        graph.title = title
+        graph.titlePlotAreaFrameAnchor = .top
+        graph.titleDisplacement = CGPoint(x: 0.0, y: 16.0)
+        
+        //Below is to reset the yAxis tick scale for the yAxis data(MW)
+        // in the 100 - 1000 grams range
+        let axisSet = graph.axisSet as! CPTXYAxisSet
+        let y = axisSet.yAxis
+        y?.majorIntervalLength = 2
+        
+        // 4 - Set up plot space
+        // Number of data sets
+        let xMin = 0.0
+        let xMax = Double(1.0)
+        // MW range limit
+        let yMin = 0.0
+        let yMax = 10.0
+        
+        guard let plotSpace = graph.defaultPlotSpace as? CPTXYPlotSpace else { return }
+        plotSpace.xRange = CPTPlotRange(locationDecimal: CPTDecimalFromDouble(xMin), lengthDecimal: CPTDecimalFromDouble(xMax - xMin))
+        plotSpace.yRange = CPTPlotRange(locationDecimal: CPTDecimalFromDouble(yMin), lengthDecimal: CPTDecimalFromDouble(yMax - yMin))
+        //print("xRange = \(plotSpace.xRange)")
+        //print("yRange = \(plotSpace.yRange)")
+    }
+    
+    func configureChart() {
+        // 1 - Set up the MW and PSA plots
+        // MW & STD
+        plot1 = CPTBarPlot()
+        plot1.fill = CPTFill(color: CPTColor(componentRed:0.28, green:0.28, blue:0.95, alpha:1.00))
+        
+        // 2 - Set up line style
+        let barLineStyle = CPTMutableLineStyle()
+        barLineStyle.lineColor = CPTColor.lightGray()
+        barLineStyle.lineWidth = 1.5
+        
+        // 3 - Add plots to graph
+        guard let graph = speedBarView.hostedGraph else { return }
+        var barX = BarInitialX
+        // MW and PSA plots:
+        let plots = [plot1!]
+        for plot: CPTBarPlot in plots {
+            plot.dataSource = self
+            plot.delegate = self
+            plot.barWidth = NSNumber(value: BarWidth)
+            plot.barOffset = NSNumber(value: barX)
+            plot.lineStyle = barLineStyle
+            graph.add(plot, to: graph.defaultPlotSpace)
+            barX += BarWidth
+        }
+        
+    }
+    
+    func configureAxes() {
+        let axisLineStyle = CPTMutableLineStyle()
+        axisLineStyle.lineWidth = 2.0
+        axisLineStyle.lineColor = CPTColor.black()
+        
+        // 2 - Get the MW and PSA graph's axis set
+        guard let axisSet = speedBarView.hostedGraph?.axisSet as? CPTXYAxisSet else { return }
+        // 3 - Configure the x-axis
+        if let xAxis = axisSet.xAxis {
+            xAxis.labelingPolicy = .none
+            xAxis.majorIntervalLength = 1
+            xAxis.axisLineStyle = axisLineStyle
+            var majorTickLocations = Set<NSNumber>()
+            var axisLabels = Set<CPTAxisLabel>()
+            let rates = ["Speed"]
+            var tickNum = 0.00
+            for (idx, rate) in rates.enumerated() {
+                tickNum = tickNum + 0.25
+                majorTickLocations.insert(NSNumber(value: idx))
+                let label = CPTAxisLabel(text: "\(rate)", textStyle: CPTTextStyle())
+                // cannot use idx because x-axis length = 1.50
+                // need to use 0, 0.25, 0.5, 0.75, 1.00, so use tickNum values
+                label.tickLocation = NSNumber(value: tickNum)
+                label.offset = 0.0
+                label.alignment = .right
+                axisLabels.insert(label)
+            }
+            xAxis.majorTickLocations = majorTickLocations
+            xAxis.axisLabels = axisLabels
+        }
+    
+        // 4 - Configure the y-axis for MW and PSA graph:
+        if let yAxis = axisSet.yAxis {
+            yAxis.labelingPolicy = .fixedInterval
+            yAxis.labelOffset = 0.0
+            yAxis.minorTicksPerInterval = 2
+            yAxis.majorTickLength = 5
+            let majorTickLineStyle = CPTMutableLineStyle()
+            majorTickLineStyle.lineColor = CPTColor.black().withAlphaComponent(0.4)
+            yAxis.majorTickLineStyle = majorTickLineStyle
+            yAxis.minorTickLength = 5
+            let minorTickLineStyle = CPTMutableLineStyle()
+            minorTickLineStyle.lineColor = CPTColor.black().withAlphaComponent(0.20)
+            yAxis.minorTickLineStyle = minorTickLineStyle
+            yAxis.axisLineStyle = axisLineStyle
+        }
+    
+    }
 }
 
+    
+
+extension ViewController: CPTBarPlotDataSource, CPTBarPlotDelegate {
+    
+    func numberOfRecords(for plot: CPTPlot) -> UInt {
+        return 1
+    }
+    
+    func number(for plot: CPTPlot, field fieldEnum: UInt, record idx: UInt) -> Any? {
+        
+        if fieldEnum == UInt(CPTBarPlotField.barTip.rawValue) {
+            if plot == plot1 {
+                return (runSpeed)
+            }
+            
+        }
+        return idx
+    }
+    
+    func barPlot(_ plot: CPTBarPlot, barWasSelectedAtRecord idx: UInt, with event: UIEvent) {
+        // 1 - Is the plot hidden?
+        if plot.isHidden == true {
+            return
+        }
+        // 2 - Create style, if necessary
+        let style = CPTMutableTextStyle()
+        style.fontSize = 6.0
+        style.fontName = "HelveticaNeue-Bold"
+        style.color = CPTColor.magenta()
+        
+        // 3 - Create annotation
+        guard let prop = number(for: plot,
+                                field: UInt(CPTBarPlotField.barTip.rawValue),
+                                record: idx) as? CGFloat else { return }
+        
+        propAnnotation?.annotationHostLayer?.removeAnnotation(propAnnotation)
+        propAnnotation = CPTPlotSpaceAnnotation(plotSpace: plot.plotSpace!, anchorPlotPoint: [0,0])
+        
+        
+        // 4 - Create number formatter
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 2
+        // 5 - Create text layer for annotation
+        let propValue = formatter.string(from: NSNumber(cgFloat: prop))
+        let textLayer = CPTTextLayer(text: propValue, style: style)
+        propAnnotation!.contentLayer = textLayer
+        
+        // 6 - Get plot index
+        var plotIndex: Int = 0
+        if plot == plot1 {
+            plotIndex = 0
+        }
+        
+        
+        // 7 - Get the anchor point for annotation
+        let x = CGFloat(idx) + CGFloat(BarInitialX) + (CGFloat(plotIndex) * CGFloat(BarWidth))
+        let y = CGFloat(prop) + 0.05
+        
+        propAnnotation!.anchorPlotPoint = [NSNumber(cgFloat: x), NSNumber(cgFloat: y)]
+        
+        // 8 - Add the annotation
+        guard let plotArea = plot.graph?.plotAreaFrame?.plotArea else { return }
+        plotArea.addAnnotation(propAnnotation)
+        
+    }
+    
+}
